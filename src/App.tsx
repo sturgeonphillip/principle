2e1b4b34c88c77a04972902d91fea82bf5c960c2
Phillip Sturgeon <sturgeonphillip@protonmail.com>
2022-03-29 01:23:02 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { render } from 'react-dom';
+// import { StrictMode } from 'react'
+
+export default function App() {
+  return <></>;
+}
+
+render(<App />, document.getElementById('root'));
